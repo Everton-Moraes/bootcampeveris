af4d0d80c999276d660b68424d222d1f1bd30edc
Everton <evertongmoraes@gmail.com>
2020-12-13 17:32:34 -0300

    master

---
+++ Java/caracteristicas-da-linguagem/src/main/java/caracteristicas/da/linguagem/Exemplo11.java
@@ -0,0 +1,17 @@
+package caracteristicas.da.linguagem;
+
+import java.time.LocalDate;
+
+/**
+ * Exemplo de como utilizar LocalDate
+ */
+public class Exemplo11 {
+    public static void main(String[] args) {
+
+        LocalDate hoje = LocalDate.now();
+
+        System.out.println(hoje);
+        // 2019-07-14
+
+    }
+}
